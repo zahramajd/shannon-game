bfbcc783f8a8412b8745f62a7f6a83cee5ff178d
zahramajd <zahra.majdabadi95@gmail.com>
2018-12-17 21:05:06 +0330

    remove punctuation

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+from hazm import word_tokenize, Lemmatizer
+import string
 
 
 def load_corpus(data_path):
@@ -5,10 +7,16 @@ def load_corpus(data_path):
             corpus = corpus.read()
     return corpus
 
-def tokenize():
-    return
+def tokenize(corpus,lemma=True, punctuation=True, space_to_space=True):
 
-def generate_n_gram():
+    if(not punctuation):
+        table = str.maketrans({key: None for key in string.punctuation})
+        corpus = corpus.translate(table)   
+
+    tokenized = word_tokenize(corpus)
+    return tokenized
+
+def generate_n_gram(tokenized):
     return
 
 def generate_sentence():
@@ -18,3 +26,5 @@ def perplexity():
     return
 
 corpus = load_corpus("corpus.txt")
+tokenized = tokenize(corpus,punctuation=False)
+print(tokenized)
