6a4442f97bef7d8f93c00ff351f9b481806ce086
zahramajd <zahra.majdabadi95@gmail.com>
2018-12-17 15:44:55 +0330

    load corpus

--- main.py
+++ main.py
@@ -1,18 +1,20 @@
 
 
-def load_corpus():
-    return
+def load_corpus(data_path):
+    with open(data_path, "r", encoding="utf-8") as corpus:
+            corpus = corpus.read()
+    return corpus
 
 def tokenize():
     return
 
-def generate-n-gram():
+def generate_n_gram():
     return
 
-def generate-sentence():
+def generate_sentence():
     return
 
 def perplexity():
     return
 
-
+corpus = load_corpus("corpus.txt")
