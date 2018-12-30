0d9561206b6eb6736ee06393539212772d8e4560
zahramajd <zahra.majdabadi95@gmail.com>
2018-12-30 10:36:56 +0330

    use models that generated before to start new models

--- main.py
+++ main.py
@@ -71,13 +71,35 @@ def generate_sentence(n, start_words, ngrams, ngrams_minus_1):
     return sentence,perplexity
 
 # n-gram
-n = 2
+n = 3
 
 corpus = load_corpus("test.txt")
 tokenized = tokenize(corpus,lemma=False, punctuation=True)
-ngrams = generate_n_gram(tokenized, n)
-ngrams_minus_1 = generate_n_gram(tokenized, n-1)
-sentence, perplexity = generate_sentence(n=n,start_words=['<S>'] ,ngrams=ngrams, ngrams_minus_1=ngrams_minus_1)
 
-print (sentence)
-print(perplexity)
+
+# ngrams = generate_n_gram(tokenized, n)
+# ngrams_minus_1 = generate_n_gram(tokenized, n-1)
+# sentence, perplexity = generate_sentence(n=n,start_words=['<S>','و'] ,ngrams=ngrams, ngrams_minus_1=ngrams_minus_1)
+# print (sentence)
+# print(perplexity)
+
+sentences = []
+
+for n in range(1,6):
+    for j in range(10):
+        print(n, j)
+        ngrams = generate_n_gram(tokenized, n)
+        ngrams_minus_1 = generate_n_gram(tokenized, n-1)
+
+        if(n==1):
+            sentence, perplexity = generate_sentence(n=n,start_words=[] ,ngrams=ngrams, ngrams_minus_1=ngrams_minus_1)
+
+        if(n==2):
+            sentence, perplexity = generate_sentence(n=n,start_words=['<S>'] ,ngrams=ngrams, ngrams_minus_1=ngrams_minus_1)
+            sentences.append(sentence)
+
+        if(not n==1 and not n==2):
+            sentence, perplexity = generate_sentence(n=n,start_words=sentences[(n-3)*10+j][0:n-1] ,ngrams=ngrams, ngrams_minus_1=ngrams_minus_1)
+            sentences.append(sentence)
+
+        print('ngram: ', n, 'sentence: ', sentence, 'perplexity: ', perplexity)
